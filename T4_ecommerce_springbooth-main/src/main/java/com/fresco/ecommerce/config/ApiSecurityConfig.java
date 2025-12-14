5798cc506fdd32bf9287d313136e8ad72cd221bf
reddy <reddypavan439@gmail.com>
2025-12-14 07:20:56 +0530

    Initial commit

---
+++ T4_ecommerce_springbooth-main/src/main/java/com/fresco/ecommerce/config/ApiSecurityConfig.java
@@ -0,0 +1,61 @@
+package com.fresco.ecommerce.config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.RegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fresco.ecommerce.service.UserAuthService;
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ApiSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private JwtAuthenticationFilter jwtAuthFilter;
+	
+	@Autowired
+	private ApiAuthenticationEntryPoint entryPoint;
+	
+	@Autowired
+	private UserAuthService userAuthService;
+	
+//	@Override
+//	public void configure(WebSecurity web) throws Exception {
+//	}
+	@Autowired
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userAuthService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf(customizer -> customizer.disable())
+		.authorizeRequests(request -> request
+				.antMatchers("/api/public/**")
+				.permitAll()
+				.antMatchers("/api/auth/consumer/**").hasAuthority("CONSUMER")
+				.antMatchers("/api/auth/seller/**").hasAuthority("SELLER")
+				.anyRequest().authenticated())
+		.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+		.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+		.exceptionHandling().authenticationEntryPoint(entryPoint);
+	}
+//	@Bean
+//	public RegistrationBean jwtAuthFilterRegister(JwtAuthenticationFilter filter) {
+//		return null;
+//	}
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+}
